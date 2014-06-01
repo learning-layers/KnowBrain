60694210f98f7f122a1b045e43e1274d06162c69
Lukas Greussing <lukas.greussing@student.tugraz.at>
2014-06-01 21:22:33 +0200

    implemented model, controller and view for qa main page

---
+++ js/app/modules/qa/qaController.js
@@ -0,0 +1,174 @@
+/**
+* Code contributed to the Learning Layers project
+* http://www.learning-layers.eu
+* Development is partly funded by the FP7 Programme of the European Commission under
+* Grant Agreement FP7-ICT-318209.
+* Copyright (c) 2014, Graz University of Technology - KTI (Knowledge Technologies Institute).
+* For a list of contributors see the AUTHORS file at the top-level directory of this distribution.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+// Encoding: UTF-8
+'use strict';
+
+/**
+* AUTHORISATION MODULE 
+*/
+angular.module('module.qa',[]);
+
+/**
+* CONFIG
+*/
+angular.module('module.qa').config(function($stateProvider) {
+
+    $stateProvider
+        .state('app.qa', {
+            url:'/qa',
+            controller: 'QAController',
+            templateUrl: MODULES_PREFIX + '/qa/qa.tpl.html'
+        });
+
+});
+
+/**
+* CONTROLLER
+*/
+angular.module('module.qa').controller("QAController", ['$scope', '$q', 'UserService', 'qaService', 'Thread','THREAD_TYPE', function($scope, $q, UserSrv, qaService, Thread, THREAD_TYPE){
+			
+		$scope.threadType = THREAD_TYPE.question;
+		$scope.threadTypeLabel = "question";
+		
+		$scope.threadResponseLabel = "answer";
+		
+		$scope.threadTitle = '';
+		$scope.threadExplanation = '';
+				
+		$scope.titlePlaceholder = '';		
+		$scope.explanationPlaceholder = '';
+		$scope.help = '';
+		
+		$scope.listHeader = "Top Threads";
+		$scope.listFiltersIsCollapsed = false;
+		
+		$scope.listType = "top";
+				
+		$scope.$watch('threadType', function(newValue, oldValue) {
+			
+			switch($scope.threadType) {
+				case THREAD_TYPE.question:
+					$scope.threadTypeLabel = "question";
+					break;
+				case THREAD_TYPE.discussion:
+					$scope.threadTypeLabel = "discussion";
+					break;
+				case THREAD_TYPE.chat:
+					$scope.threadTypeLabel = "chat";
+					break;
+				}
+
+				$scope.titlePlaceholder = "Enter title of your " + $scope.threadTypeLabel;	
+				$scope.explanationPlaceholder = "Add an explanation to your " + $scope.threadTypeLabel;			
+		});
+		
+		$scope.addNewThread = function(type, title, explanation)
+		{
+			var thread = new Thread(UserSrv.getUserUri(), type, title, explanation, '', '', new Date().getTime());
+			
+			addNewThread(thread)
+				.then(loadAuthorDetails);
+		};
+		
+		$scope.getRandomNumber = function(max)
+		{
+			return Math.floor((Math.random() * max) + 1);
+		};
+		
+		$scope.showHelp = function(focusedControl)
+		{
+			switch(focusedControl) {
+					case 'title':
+						$scope.help = "Your " + $scope.threadTypeLabel + " title is the first thing another user sees. It should be clear, concise, and contain enough information so that others can see what it’s about at first glance";
+						break;
+					case 'explanation':
+						$scope.help = "Provide any details that other researchers might need to discuss your topic.";
+						break;
+			}
+		};
+		
+		var loadThreadList = function() 
+		{
+			return qaService
+							.getAllThreads()
+							.then(function(result) {
+								$scope.threadList = result;
+								return result;
+							});
+		};
+				
+		var loadThreadEntries = function(thread) 
+		{
+			return qaService
+							.getThreadEntries(thread)
+							.then(function(result) {
+								$scope.threadEntries = result;
+								return result;
+							});
+		};
+		
+		var loadAuthorDetails = function(threadList) 
+		{
+			angular.forEach(threadList, function(value, key){
+				qaService
+							.getAuthorDetails(value)
+							.then(function(result) {
+								value.author = result;
+								return value.author;
+							});
+			});
+			
+			return 
+		};
+		
+		var addNewThread = function(thread)
+		{
+			return qaService
+							.addNewThread(thread)
+							.then(function(result) {
+								thread.uri = result;
+								$scope.threadList.push(thread);
+								var tmpList = new Array();
+								tmpList.push(thread);
+								return tmpList;
+							});
+		};	
+
+		// load data
+		loadThreadList()
+			.then(loadAuthorDetails);		
+			
+}]);
+
+/**
+* Directives
+*/
+angular.module('module.qa').directive('bindOnce', function() {
+    return {
+        scope: true,
+        link: function( $scope ) {
+            setTimeout(function() {
+                $scope.$destroy();
+            }, 0);
+        }
+    }
+});
---
+++ js/app/modules/qa/qaModel.js
@@ -0,0 +1,245 @@
+/**
+* Code contributed to the Learning Layers project
+* http://www.learning-layers.eu
+* Development is partly funded by the FP7 Programme of the European Commission under
+* Grant Agreement FP7-ICT-318209.
+* Copyright (c) 2014, Graz University of Technology - KTI (Knowledge Technologies Institute).
+* For a list of contributors see the AUTHORS file at the top-level directory of this distribution.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+ 
+// Encoding: UTF-8
+'use strict';
+
+/************************************************************************************
+* Q&A - Model and Services
+* TODO: Probably this part should be outsourced to a separate file  
+*************************************************************************************/
+
+/**
+* Model
+*/
+
+// type of the thread; either disc, qa or chat
+angular.module('module.models').constant('THREAD_TYPE', {discussion:'disc', question:'qa', chat:'chat'});
+
+angular.module('module.models').factory('Thread', function () {
+ 		
+  // Constructor
+  function Thread(author_uri, type, title, explanation, target_entry_uri, uri, creationTimeTicks) {
+    // Public properties
+		this.authorUri = author_uri;
+		// type of the thread; either disc, qa or chat
+    this.type = type;
+		this.title = title;
+		this.explanation = explanation;
+		// target uri of the entity to start a thread for
+		this.targetUri = target_entry_uri;
+		this.uri = uri;
+		this.entries = new Array();
+		this.author = null;
+		this.creationTime = new Date(creationTimeTicks);
+  }
+ 
+ /*
+  // Public method, assigned to prototype
+  User.prototype.getFullName = function () {
+    return this.firstName + ' ' + this.lastName;
+  };
+ 
+  // Private property
+  var possibleRoles = ['admin', 'editor', 'guest'];
+ 
+  // Private function
+  function checkRole(role) {
+    return possibleRoles.indexOf(role) !== -1;
+  }
+ 
+  
+  // Static property
+  // Using copy to prevent modifications to private property
+  User.possibleRoles = angular.copy(possibleRoles);
+ 
+  // Static method, assigned to class
+  // Instance ('this') is not available in static context
+  User.build = function (data) {
+    if (!checkRole(data.role)) {
+      return;
+    }
+    return new Person(
+      data.first_name,
+      data.last_name,
+      data.role,
+      Organisation.build(data.organisation) // another model
+    );
+  };
+ */
+  // Return the constructor function
+  return Thread;
+})
+
+angular.module('module.models').factory('ThreadEntry', function () {
+ 		
+  // Constructor
+  function ThreadEntry(author_uri, content, disc_entry_type, position, timestamp, uri) {
+    // Public properties
+		this.authorUri = author_uri;
+		this.content = content;
+		// type of the thread; either discEntry, qaEntry or chatEntry
+    this.type = disc_entry_type;
+		this.position = position;
+		this.timestamp = timestamp;
+		this.uri = uri;
+  }
+	
+	return ThreadEntry;
+})
+
+angular.module('module.models').factory('Author', function () {
+ 		
+  // Constructor
+  function Author(name) {
+    // Public properties
+		this.name = name;
+  }
+	
+	return Author;
+})
+
+/**
+* Services
+*/
+
+angular.module('module.models').service("qaService", ['$q', '$rootScope','UserService','Thread','ThreadEntry','Author', function($q, $rootScope, UserSrv, Thread, ThreadEntry, Author){
+
+	this.addNewThread = function(thread){
+		var defer = $q.defer();
+		
+		new SSDiscEntryAdd(
+			function(result){
+				defer.resolve(result.disc);
+			},
+			function(error){
+				console.log(error);
+				defer.reject(error);
+			},
+			UserSrv.getUserUri(),
+			UserSrv.getKey(),
+			thread.uri,
+			thread.targetUri,
+			'',
+			true,
+			thread.type,
+			thread.title,
+			thread.explanation
+			);
+
+		return defer.promise;     
+	};
+	
+	this.addComment = function(thread, comment){
+		var defer = $q.defer();
+
+		new SSDiscEntryAdd(
+			function(result){
+				defer.resolve(result);
+			},
+			function(error){
+				console.log(error);
+				defer.reject(error);
+			},
+			UserSrv.getUserUri(),
+			UserSrv.getKey(),
+			thread.uri,
+			thread.targetUri,
+			comment,
+			false,
+			thread.type,
+			thread.title
+			);
+
+		return defer.promise;     
+	};
+
+	this.getThreadEntries = function(thread){
+		var defer = $q.defer();
+		var entries = new Array();
+		
+		new SSDiscWithEntriesGet(
+			function(result){
+				angular.forEach(result.disc.entries, function(value, key){
+					var entry = new ThreadEntry(value.author, value.content, value.discEntryType, value.pos, value.timestamp, value.uri, value.creationTime);
+					entries.push(entry);
+				});
+
+        thread.entries = entries;
+        defer.resolve(thread);
+      },
+      function(error){
+			  console.log(error);
+				defer.reject(error);
+      },
+      UserSrv.getUserUri(),
+      UserSrv.getKey(),
+			thread.uri
+      );
+
+    return defer.promise;     
+	};
+
+  this.getAllThreads = function(){
+    var defer = $q.defer();
+		var threadList = new Array();
+		
+    new SSDiscsAllGet(
+      function(result){
+        angular.forEach(result.discs, function(value, key){
+					var thread = new Thread(value.author, value.discType, value.label, value.explanation, value.target, value.uri, value.creationTime);
+          threadList.push(thread);
+        });
+
+        defer.resolve(threadList);
+      },	
+      function(error){
+			  console.log(error);
+				defer.reject(error);
+      },
+      UserSrv.getUserUri(),
+      UserSrv.getKey()
+      );
+
+    return defer.promise;  
+  };
+	
+	this.getAuthorDetails = function(thread){
+		var defer = $q.defer();
+
+		new SSEntityGet(
+			function(result){
+				var author = new Author(result.entity.label);
+				defer.resolve(author);
+			},
+			function(error){
+				console.log(error);
+				defer.reject(error);
+			},
+			UserSrv.getUserUri(),
+			UserSrv.getKey(),
+			thread.authorUri
+			);
+
+		return defer.promise;     
+	};
+		
+}]);
