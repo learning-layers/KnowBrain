7d12f7ed96748854e515282148c2310e766279f0
Matthias Rella <git@myrho.net>
2014-08-20 11:37:38 +0200

    using UserService to setup SocialSemanticService

--- js/app/modules/data/sss/CollectionData.js
+++ js/app/modules/data/sss/CollectionData.js
@@ -8,17 +8,14 @@ angular.module('adapter').factory('CollectionData', function() {
     Logger.get("adapter").debug("CollectionData", CollectionData);
     return CollectionData;//require('data/sss/CollectionData');
 });
-angular.module('adapter').factory('DataStore', ['CollectionData', function(CollectionData) {
+angular.module('adapter').factory('DataStore', ['CollectionData', 'UserService', function(CollectionData, UserSrv) {
     var vie = new VIE();
-    var space = "armin";
-    var app = "kb";
-    var username = "pjotr";
     
-    var namespace = "http://"+space+"."+app+"/";
+    var namespace = UserSrv.getUserSpace() + "/";
     var userParams = {};
-    userParams.user = namespace + 'user/'+username;
-    userParams.ueTrackUser = namespace + 'user/ue_track_'+username;
-    userParams.userKey = "681V454J1P3H4W3B367BB79615U184N22356I3E";
+    userParams.user = UserSrv.getUser();
+    userParams.ueTrackUser = "";
+    userParams.userKey = UserSrv.getKey();
         
     var sss = new SocialSemanticService(_.extend({
         'namespaces': {
