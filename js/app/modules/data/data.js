30b69b9b0c2a2ed949f50b19594dfdb5ddcd1e54
Matthias Rella <git@myrho.net>
2014-08-20 17:25:28 +0200

    cleanup of vieMap directive

--- js/app/modules/data/data.js
+++ js/app/modules/data/data.js
@@ -45,75 +45,66 @@ angular.module('data').directive("vieMap", function() {
     return {
         restrict: 'A',
         priority: 1001,
+        // parses a directive like "x[sss:y] as z"
+        // or "x[sss:y] as [zz]" where zz should become an array
+        // and maps a VIE entity in the scope to a plain object or array of objects
         link: function(scope, element, attrs ){
-            Logger.useDefaults();
-
             var mapAsArray = function(value) {
-                LOG.debug('mapAsArray', value, value.indexOf('['), value.indexOf(']'));
                 return value.indexOf('[') === 0 &&
                     value.indexOf(']') === value.length -1;
             };
 
-            LOG.setLevel(Logger.DEBUG);
-            LOG.debug('scope', scope);
-            LOG.debug('element', element);
-            LOG.debug('attrs', attrs);
-
             var map = attrs[attr].split(sep);
-            LOG.debug('map', map);
             var key_parts = map[0].split("[");
-            LOG.debug('key_parts', key_parts);
+            
+            // key corr. to "x"
             var key = key_parts[0];
+
+            // keykey corr. to "sss:y"
             var keykey = key_parts[1].substring(0, key_parts[1].length-1);
-            LOG.debug('key', key, 'keykey', keykey, scope[key]);
             if( !scope[key] ) return;
 
-            var value = scope[key].get(keykey) || [];
-            if( !_.isArray(value)) value = [value];
+            var getValue = function(key, keykey) {
+                // get the value from the scoped VIE entity and normalize it to an array
+                var value = scope[key].get(keykey) || [];
+                if( !_.isArray(value)) value = [value];
 
-            var newValue = [];
-            _.each(value, function(v) {
-                if( v.isEntity ) {
-                    newValue.push(scope[key].vie.service('sss').fixFromVIE(v));
-                }
-            });
-            value = newValue;
-
-            var valueKey = map[1];
-            if( mapAsArray(map[1]) ) {
-                valueKey = map[1].substring(1, map[1].length -1);
-            }
-            LOG.debug('valueKey', valueKey);
-            if( !mapAsArray(map[1]) ) {
-                value = value[0];
-            } 
-            scope[valueKey] = value;
-
-            var changeEventName = 'change:' + scope[key].vie.namespaces.uri(keykey);
-
-            var eventMapper = function(model, value, options) {
-                LOG.debug("changing", valueKey, scope[valueKey], scope[valueKey].length);
-                var current = scope[key].get(keykey) || [];
-                if( !_.isArray(current)) current = [current];
-                var newCurrent = [];
-                _.each(current, function(v) {
+                var newValue = [];
+                // if the value is an VIE entity also (or an array of) turn it in an object
+                _.each(value, function(v) {
                     if( v.isEntity ) {
-                        newCurrent.push(scope[key].vie.service('sss').fixFromVIE(v));
+                        // using SocialSemanticService
+                        newValue.push(scope[key].vie.service('sss').fixFromVIE(v));
                     }
                 });
-                current = newCurrent;
-                if( !mapAsArray(map[1]) ) {
-                    current = current[0];
+                value = newValue;
+
+                // map[1] corr. to "z" or "[zz]"
+                // valueKey corr. to "zz" without []
+                var valueKey = map[1];
+                if( mapAsArray(map[1]) ) {
+                    valueKey = map[1].substring(1, map[1].length -1);
                 }
-                scope[valueKey] = current;
-                LOG.debug("changed", scope[valueKey], scope[valueKey].length);
+                if( !mapAsArray(map[1]) ) {
+                    value = value[0];
+                } 
+                scope[valueKey] = value;
+            };
+
+            // call it on initalization of the link
+            getValue(key, keykey);
+
+            // setup the change listener for the VIE entity
+            var changeEventName = 'change:' + scope[key].vie.namespaces.uri(keykey);
+
+            var eventMapper = function() {
+                // call getValue on change of the entity
+                getValue(key, keykey);
                 scope.$digest();
             };
 
-            LOG.debug('uri', scope[key].vie.namespaces.uri(keykey));
             scope[key].on(changeEventName, eventMapper);
             scope.$on('$destroy',function(e) {
-                LOG.debug('scope destroy e=',e);
                 if( e.targetScope !== scope ) return; 
                 scope[key].off(changeEventName, eventMapper);
             });
