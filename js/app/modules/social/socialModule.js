73e399489101a2abc4fb11c31ac27fe62d632401
Peter Marton <petermarton@peters-imac.know.know-center.at>
2015-04-14 14:32:33 +0200

    user profile

--- js/app/modules/social/socialModule.js
+++ js/app/modules/social/socialModule.js
@@ -36,23 +36,10 @@ angular.module('module.social').config(function($stateProvider) {
     $stateProvider
         .state('app.social', {
              url:'/social/*profileId',
-            abstract:true,
             controller: 'SocialController',
             templateUrl: MODULES_PREFIX + '/social/social.tpl.html'
         });
     
-    $stateProvider.state('app.social.activities', {
-        url: '/activities',
-        templateUrl: MODULES_PREFIX + '/social/activities.tpl.html',
-        controller: "UserActivitiesController"
-    });
-
-    $stateProvider.state('app.social.groups', {
-        url: '/groups',
-        templateUrl: MODULES_PREFIX + '/social/groups.tpl.html',
-        controller: "GroupsController as groupCtrl"
-    });
-
     $stateProvider.state('app.social.friends', {
         url: '/friends',
         templateUrl: MODULES_PREFIX + '/social/friends.tpl.html',
@@ -73,86 +60,24 @@ angular.module('module.social').controller("SocialController", ['$scope', '$stat
     });
     
     $scope.userId = UserSrv.getUser();
-    
-    $scope.tabs = 
-        [
-           { heading: "Activities", route:"app.social.activities", active:false },
-           { heading: "Groups", route:"app.social.groups", active:false },
-        ];
-    
-    $scope.go = function(route){
-        $state.go(route);
-    };
- 
-    $scope.active = function(route){
-        return $state.is(route);
-    };
- 
-    $scope.$on("$stateChangeSuccess", function() {
-        $scope.tabs.forEach(function(tab) {
-            tab.active = $scope.active(tab.route);
-        });
-    });
-}]);
+    $scope.isFriend = false;
 
-
-angular.module('module.social').controller("UserActivitiesController", ['$scope', 'Activity', 'ActivityFetchService', function($scope, Activity, ActivityFetchService){
-    
-    var promise = ActivityFetchService.getActivities(null, [$scope.profileId], null, null, null, null);
-    
-    $scope.activities = [];
-    
+    var promise = UserFetchService.getFriends();
     promise.then(function(result) {
-        
-        for(var i = 0; i < result.activities.length; i++) {
-            var act = result.activities[i];
-            
-            var activity = new Activity(act.author, act.type, act.creationTime, act.entities, act.users);
-            $scope.activities.unshift(activity);
+        for (var i = 0; i < result.friends.length; i++) {
         }
-    });
-}]);
-
-angular.module('module.social').controller("GroupsController", ['$scope', '$state', '$controller', '$dialogs', 'GroupFetchService', function($scope, $state, $controller, $dialogs, GroupFetchService){
-    
-    $scope.groups = [];
-    
-    var promise = GroupFetchService.getUserGroups($scope.profileId);
-    
-    promise.then(function(result){
-        $scope.groups = result.circles;
+        $scope.isFriend = true;
     });
     
-
-    this.createGroup = function() {
-        
-        var states = {
-                "new": MODULES_PREFIX+"/group/newGroup.tpl.html",
-                "members": MODULES_PREFIX+"/group/addMembers.tpl.html",
-                "choose": MODULES_PREFIX+"/group/addEntities.tpl.html",
-                "upload": MODULES_PREFIX+"/group/addEntitiesUpload.tpl.html",
-                "link": MODULES_PREFIX+"/group/addEntitiesLink.tpl.html"
-                //TODO: Add from collection
-                
-                
-        };
-        
-        var ctrlFunction = function($scope) {
-            $scope.groupMembers = [];
-            $scope.entities = [];
-        };
-        
-        $dialogs.newModal(states, ctrlFunction, "modal-huge");
-
-    };
-    
-    $scope.handleEntryClick = function(entry) {
-        if(entry.type == "circle") {
-            $state.go("app.group.activities", {groupId: entry.id});
-        }
-    };
-
+    $scope.addAsFriend = function() {
+        var promise = UserFetchService.addFriend($scope.userId);
+        promise.then(function(result) {
+            $scope.isFriend = true;
+        });
+    }
 }]);
+
+
 
 angular.module('module.social').controller("FriendsController", ['$scope',function($scope){
     //TODO: List friends
